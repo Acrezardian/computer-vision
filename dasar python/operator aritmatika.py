c1bedcd76377a5ba783956a9396886c4140c35df
Acrezardian <129355710+Acrezardian@users.noreply.github.com>
2023-10-17 20:38:33 +0700

    Create operator aritmatika.py

---
+++ dasar python/operator aritmatika.py
@@ -0,0 +1,27 @@
+# ambil input untuk mengisi nilai
+a = int(input("Masukkan nilai a : "))
+b = int(input("Masukkan nilai b : "))
+
+# operator penjumlahan
+c = a + b
+print ("hasil %d + %d = %d" % (a,b,c))
+
+# operator pengurangan
+c = a - b
+print ("hasil %d - %d = %d" % (a,b,c))
+
+# operator perkalian
+c = a * b
+print ("hasil %d * %d = %d" % (a,b,c))
+
+# operator pembagian
+c = a / b
+print ("hasil %d / %d = %d" % (a,b,c))
+
+# operator penjumlahan
+c = a % b
+print ("hasil %d %% %d = %d" % (a,b,c))
+
+# operator penjumlahan
+c = a ** b
+print ("hasil %d ** %d = %d" % (a,b,c))
