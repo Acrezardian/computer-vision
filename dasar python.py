6562bc8d37b66b2f61a470d2c9838c7caa8745fc
Acrezardian <129355710+Acrezardian@users.noreply.github.com>
2023-10-17 20:16:24 +0700

    Update dasar python.py

--- dasar python.py
+++ dasar python.py
@@ -1,3 +1,38 @@
 print("hello world!")
 print (2 + 3)
 print ("value : " + str(2))
+
+"""
+1. Penulisan Statement Python
+   Penulisan satu statement tidak diakhiri dengan tanda titik-koma.
+2. Penulisan String pada Python
+   Bisa menggunakan tanda petik tunggal maupun ganda.
+3. Penuilsan Case pada Python
+   Sintak Python bersifat case sensitive, artinya teksini dengna TeksInidibedakan.
+4. Penulisan Blok Program Python
+   Blok program adalah kumpulan dari beberpaa statement yang digabungkan dalam satu blok.
+   Penulisan blok program harus ditambahkan indentasi (tab atau spasi 2x/4x).
+5. Cara Penulisan Komentar pada Python
+   Dapat menggunakan tanda pagar (#), tanda petik (‘/”), triple petik (‘’’ / “””)
+
+Aturan Penulisan Variabel
+• Nama variabel boleh diawali menggunakan huruf atau garis bawah (_),
+  contoh: nama, _nama, namaKu, nama_variabel.
+• Karakter selanjutnya dapat berupa huruf, garis bawah (_) atau angka,
+  contoh: __nama, n2, nilai1.
+• Karakter pada nama variabel bersifat sensitif (case-sensitif). Artinya huruf besar dan kecil dibedakan. Misalnya, variabel_Ku dan variabel_ku, keduanya adalah variabel yang berbeda.
+• Nama variabel tidak boleh menggunakan kata kunci yang sudah ada dalam python seperti if, while, for, dsb.
+
+Python akan seara otomatis mengenali jenis data atau tipe data yang tersimpan dalam sebuah variabel.
+
+Fungsi-fungsi untuk mengubah tipe data:
+int() untuk mengubah menjadi integer;
+long() untuk mengubah menjadi integer panjang;
+float() untuk mengubah menjadi float;
+bool() untuk mengubah menjadi boolean;
+chr() untuk mengubah menjadi karakter;
+str() untuk mengubah menjadi string.
+bin() untuk mengubah menjadi bilangan Biner.
+hex() untuk mengubah menjadi bilangan Heksadesimal.
+oct() untuk mengubah menjadi bilangan okta.
+"""
