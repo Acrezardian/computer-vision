24e1776ca57cd2913d9fbdc4a4f9740365e1569a
Acrezardian <129355710+Acrezardian@users.noreply.github.com>
2023-10-17 21:04:51 +0700

    Create operator logika.py

---
+++ dasar python/operator logika.py
@@ -0,0 +1,16 @@
+﻿а = True
+b = False
+
+# Logika AND 
+c = a and b
+print ("%r and %r = %n" % (a,b,c))
+
+# Logika OR
+c = a or b
+print ("%r or %r = %r" % (a,b,c))
+
+# Logika Not
+c = not a print ("not %r = %n" % (a,c))
+
+# Logika Nor
+c = nor a print ("not %r = %n" % (a,c))
