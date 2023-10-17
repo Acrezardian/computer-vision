e1cbf29ad77546c26f41f13eef29250aed40e597
Acrezardian <129355710+Acrezardian@users.noreply.github.com>
2023-10-17 21:22:35 +0700

    Create looping.py

---
+++ dasar python/looping.py
@@ -0,0 +1,12 @@
+# perulangan FOR
+ulang = 10
+for i in range (ulang):
+   print ("perulangan ke-" + str(i))
+
+# perulangan WHILE
+﻿jawab = 'ya' 
+hitung = 0
+while(jawab == 'ya'):
+   hitung += 1 
+   jawab = input("Apakah akan di ulang? ")
+print ("Total perulagan : " + str(hitung))
