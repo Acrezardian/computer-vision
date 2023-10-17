798559a0068de3f1c1844ea8bd090a1e8dfbb34d
Acrezardian <129355710+Acrezardian@users.noreply.github.com>
2023-10-17 20:51:12 +0700

    Create operator penugasan.py

---
+++ dasar python/operator penugasan.py
@@ -0,0 +1,9 @@
+# Ambil input untuk mengisi nilai
+a = int(input("masukkan nilai a : "))
+print ("nilai a = %d" % a)
+
+# Jumlahkan nilai a dengan operator penugasan
+a += 5 # operator penugasan untuk penjumlahan 
+
+print (" Nilai a setelah ditambah 5 : ")
+print ("a = %d" % a)
