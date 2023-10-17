30fbd24f91101c46e6bd472d14c8cc58c3f6d4f0
Acrezardian <129355710+Acrezardian@users.noreply.github.com>
2023-10-17 20:23:34 +0700

    input output.py

---
+++ input output.py
@@ -0,0 +1,8 @@
+# Python sudah menyediakan fungsi input() untuk mengambil inputan dari keyboard.
+
+# Mengambil input
+nama = input("Masukkan nama kamu : ")
+umur = input("Masukkan umur kamu : ")
+
+# Menampilkan output
+print ("Hai, ",nama, " umur kamu adalah ",umur, "tahun")
