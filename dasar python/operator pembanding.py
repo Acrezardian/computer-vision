b26ecf9a4a90371b02e96f664934a033662082e9
Acrezardian <129355710+Acrezardian@users.noreply.github.com>
2023-10-17 21:01:12 +0700

    Create operator pembanding.py

---
+++ dasar python/operator pembanding.py
@@ -0,0 +1,27 @@
+a = int(input("Inputkan nilai a: ")) 
+b = int(input("Inputkan nilai b: "))
+
+# apakah a sama dengan b?
+c = a == b
+print ("Apakah %d == %d : %r" % (a,b,c))
+
+# apakah a < b?
+c = a < b
+print ("Apakah %d < %d : %r" % (a,b,c))
+
+﻿
+# apakah a > b? 
+c = a > b
+print ("Apakah %d > %d: %r" % (a,b,c))
+
+# apakah a <= b?
+c = a <= b
+print ("Apakah %d <= %d: %r" % (a,b,c))
+
+# apakah a >= b?
+c = a >= b
+print ("Apakah %d >= %d: %r" % (a,b,c))
+
+# apakah a != b?
+c = a != b
+print ("Apakah %d != %d: %r" % (a,b,c))
